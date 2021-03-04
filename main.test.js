fadbd43186c882c75d3f248640b070ad516e1bd8
John Meneses <jmmenes90@gmail.com>
2021-03-04 02:58:31 -0500

    Finished.

--- main.test.js
+++ main.test.js
@@ -2,88 +2,223 @@
  * YOUR CODE HERE *
  ******************/
 
+// addAll - given an array of numbers, returns the sum of all numbers added together.
+// HINT: this is a reduce problem, reducing a collection of values to one value.
+// In this case, a collection of numbers to one total sum.
+function addAll(array) {
+  let sum = 0;
 
+  for (const value of array) {
+    // sum = sum + value;
+    sum += value;
+  }
+  return sum;
+}
 
+// countSpaces - given a string, returns the number of characters that are spaces.
+// HINT: this is a reduce problem, reducing a collection of values to one value.
+function countSpaces(string) {
+  let spaces = 0;
 
+  for (const char of string) {
+    if (char === " ") {
+      spaces += 1;
+    }
+  }
+  return spaces;
+}
 
+// countTrues - given an array of booleans, returns the number of true values.
+// HINT: this is a reduce problem, reducing a collection of values to one value.
+function countTrues(array) {
+  let trueVal = 0;
 
+  for (const boolean of array) {
+    if (boolean === true) {
+      trueVal += 1;
+    }
+  }
+  return trueVal;
+}
+
+// makeFriendly - given a paragraph, returns a paragraph where each sentence ends with
+// an exclamation point instead of the period it held before. HINT: this is a string-building
+// problem, and you'll want to mostly append to the string you'll return each character,
+// except when you hit a period.
+function makeFriendly(paragraph) {
+  let newEnd = "";
+
+  for (const char of paragraph) {
+    if (char === ".") {
+      newEnd += "!";
+    } else {
+      newEnd += char;
+    }
+  }
+  return newEnd;
+}
+
+// Alternate
+// function makeFriendly(paragraph) {
+//   return paragraph.split('.').join('!');
+// }
+
+// Alternate
+// countSpaces = (str) => {
+//   return str.split(" ").length - 1;
+//   };
+
+// `cubeAll` - given an array of numbers, returns an array where each number has been
+// cubed (a.k.a. raised to the third power).
+function cubeAll(array) {
+  // create empty array
+  let cubedNum = [];
+
+  for (const number of array) {
+    const cubed = Math.pow(number, 3);
+    cubedNum.push(cubed);
+  }
+  // return array
+  return cubedNum;
+}
+
+/* `addNoises` - given an array of strings, returns an array where each string has appended 
+to it that animal's noise. See tests for examples! **HINT:** You're definitely going to 
+have to check for each tested-for animal name, but you could use a compound boolean in 
+your `if` check, or set up an array of your own animals and use `.includes`, or use a 
+`switch` statement. Either way, you can look at the tests to see which names should 
+be considered which animals! */
+function addNoises(array) {
+  // create empty array
+  let output = [];
+
+  for (const animalName of array) {
+    // Dogs
+    if (
+      animalName === "Fido" ||
+      animalName === "Rolph" ||
+      animalName === "Maisie"
+    ) {
+      output.push(animalName + ' says "Woof!"');
+    }
+
+    // Cats
+    if (animalName === "Garfield" || animalName === "Heathcliff") {
+      output.push(animalName + ' says "Meow!"');
+    }
+
+    // dinos
+    if (animalName === "Barnie" || animalName === "Sharp Tooth") {
+      output.push(animalName + ' says "ROWR."');
+    }
+  }
+
+  return output;
+}
+
+/* `womenOnly` - given an array of strings with names and gender, returns an array of only those
+ strings where the gender (the last character in the string) is 'F'. **HINT:** you can check the 
+character at last index of the string to see if it's an `F`, but there's also a string method 
+ that tells you if a string "ends with" another string. [Duck it!](https://duckduckgo.com/) */
+
+function womenOnly(array) {
+  let output = [];
+
+  for (const person of array) {
+    const lastChar = person[person.length - 1];
+    if (lastChar === "F") {
+      output.push(person);
+    }
+  }
+  return output;
+}
+
+/* `integersOnly` - given an array of numbers, returns an array of only those numbers that 
+are integers. **HINT:** you can use modulus for this, it's just SEEMS weird to do so. */
+
+function integersOnly(array) {
+  let output = [];
+  for (const number of array) {
+    if (number % 2 === 0 || number % 2 === 1 || number % 2 === -1) {
+      output.push(number);
+    }
+  }
+  return output;
+}
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
  *********************************/
 
-
-
-if (typeof addAll === 'undefined') {
+if (typeof addAll === "undefined") {
   addAll = undefined;
 }
 
-if (typeof countSpaces === 'undefined') {
+if (typeof countSpaces === "undefined") {
   countSpaces = undefined;
 }
 
-if (typeof countTrues === 'undefined') {
+if (typeof countTrues === "undefined") {
   countTrues = undefined;
 }
 
-if (typeof makeFriendly === 'undefined') {
+if (typeof makeFriendly === "undefined") {
   makeFriendly = undefined;
 }
 
-if (typeof cubeAll === 'undefined') {
+if (typeof cubeAll === "undefined") {
   cubeAll = undefined;
 }
 
-if (typeof addNoises === 'undefined') {
+if (typeof addNoises === "undefined") {
   addNoises = undefined;
 }
 
-if (typeof womenOnly === 'undefined') {
+if (typeof womenOnly === "undefined") {
   womenOnly = undefined;
 }
 
-if (typeof integersOnly === 'undefined') {
+if (typeof integersOnly === "undefined") {
   integersOnly = undefined;
 }
 
-
-describe('addAll', () => {
+describe("addAll", () => {
   it(`adds all the numbers in the array`, () => {
     const nums1 = [5, 3, 7, -11, 1003];
     const nums2 = [8, 1, 2, -53, -812];
 
     expect(addAll(nums1)).toBe(1007);
     expect(addAll(nums2)).toBe(-854);
-  })
-})
+  });
+});
 
 describe(`countSpaces`, () => {
   it(`counts all the spaces in a multi-space string`, () => {
-    const sentence1 = 'well hello there my crumpet';
-    const sentence2 = 'strings 4 eva';
+    const sentence1 = "well hello there my crumpet";
+    const sentence2 = "strings 4 eva";
 
     expect(countSpaces(sentence1)).toBe(4);
     expect(countSpaces(sentence2)).toBe(2);
-  })
+  });
 
   it(`correctly identifies strings with no spaces`, () => {
-    const sentence1 = 'supercalifragilisticexpialidocious';
-    const sentence2 = 'antidisestablishmentarianism';
-    const sentence3 = 'pneunultramicroscopicsilicovulcanoconiosis';
+    const sentence1 = "supercalifragilisticexpialidocious";
+    const sentence2 = "antidisestablishmentarianism";
+    const sentence3 = "pneunultramicroscopicsilicovulcanoconiosis";
 
     expect(countSpaces(sentence1)).toBe(0);
     expect(countSpaces(sentence2)).toBe(0);
     expect(countSpaces(sentence3)).toBe(0);
-  })
+  });
 
   it(`doesn't count all non-letters as spaces`, () => {
-    const sentence1 = 'how now, brown cow';
-    const sentence2 = '@(*@ are you even talking about?';
+    const sentence1 = "how now, brown cow";
+    const sentence2 = "@(*@ are you even talking about?";
 
     expect(countSpaces(sentence1)).toBe(3);
     expect(countSpaces(sentence2)).toBe(5);
-  })
-})
+  });
+});
 
 describe(`countTrues`, () => {
   it(`counts all the trues in a true-only array`, () => {
@@ -92,7 +227,7 @@ describe(`countTrues`, () => {
 
     expect(countTrues(bools1)).toBe(3);
     expect(countTrues(bools2)).toBe(5);
-  })
+  });
 
   it(`counts all the trues in an array of assorted booleans`, () => {
     const bools1 = [true, false, true, false];
@@ -100,28 +235,28 @@ describe(`countTrues`, () => {
 
     expect(countTrues(bools1)).toBe(2);
     expect(countTrues(bools2)).toBe(4);
-  })
+  });
 
   it(`doesn't confuse truthy values with true values`, () => {
-    const values1 = [true, 'hello', 3, true, []]
+    const values1 = [true, "hello", 3, true, []];
 
     expect(countTrues(values1)).toBe(2);
-  })
+  });
 
   it(`doesn't confuse the string 'true' with the boolean true`, () => {
-    const values1 = [true, 'true', 'true', true, 'true'];
-    const values2 = [true, 'true', 'true', true, true, 'true', 'true', true];
+    const values1 = [true, "true", "true", true, "true"];
+    const values2 = [true, "true", "true", true, true, "true", "true", true];
 
     expect(countTrues(values1)).toBe(2);
     expect(countTrues(values2)).toBe(4);
-  })
-})
+  });
+});
 
-describe('makeFriendly', () => {
+describe("makeFriendly", () => {
   it(`given a paragraph, returns a paragraph where each sentence ends with an exclamation point instead of the period it held before.`, () => {
     const paragraph1 = `Do you see the story. Do you see anything. It seems to me I am trying to tell you a dream.`;
     const paragraph2 = `Like a running blaze on a plain, like a flash of lightning in the clouds. We live in the flicker.`;
-      const paragraph3 = `What is that feeling when you’re driving away from people and they recede on the plain till you see their specks dispersing. It’s the too-huge world vaulting us, and it’s good-bye. But we lean forward to the next crazy venture beneath the skies. Nothing behind me, everything ahead of me, as is ever so on the road.`;
+    const paragraph3 = `What is that feeling when you’re driving away from people and they recede on the plain till you see their specks dispersing. It’s the too-huge world vaulting us, and it’s good-bye. But we lean forward to the next crazy venture beneath the skies. Nothing behind me, everything ahead of me, as is ever so on the road.`;
 
     const friendly1 = `Do you see the story! Do you see anything! It seems to me I am trying to tell you a dream!`;
     const friendly2 = `Like a running blaze on a plain, like a flash of lightning in the clouds! We live in the flicker!`;
@@ -130,10 +265,10 @@ describe('makeFriendly', () => {
     expect(makeFriendly(paragraph1)).toEqual(friendly1);
     expect(makeFriendly(paragraph2)).toEqual(friendly2);
     expect(makeFriendly(paragraph3)).toEqual(friendly3);
-  })
-})
+  });
+});
 
-describe('cubeAll', () => {
+describe("cubeAll", () => {
   it(`given an array of numbers, returns an array where each number has been cubed`, () => {
     const nums1 = [3, 2, 5];
     const cubes1 = [27, 8, 125];
@@ -142,7 +277,7 @@ describe('cubeAll', () => {
 
     expect(cubeAll(nums1)).toEqual(cubes1);
     expect(cubeAll(nums2)).toEqual(cubes2);
-  })
+  });
 
   it(`doesn't modify the original array`, () => {
     const nums1 = [3, 2, 5];
@@ -155,141 +290,114 @@ describe('cubeAll', () => {
 
     expect(nums1).toEqual(originalNums1);
     expect(nums2).toEqual(originalNums2);
-  })
-})
+  });
+});
 
-describe('addNoises', () => {
-    it('adds the appropriate noises for each animal in the given array', () => {
-    const dogs = [
-      'Fido',
-      'Rolph',
-      'Maisie',
+describe("addNoises", () => {
+  it("adds the appropriate noises for each animal in the given array", () => {
+    const dogs = ["Fido", "Rolph", "Maisie"];
+
+    const cats = ["Garfield", "Heathcliff"];
+
+    const dinos = ["Barnie", "Sharp Tooth"];
+
+    const animals1 = ["Fido", "Garfield", "Sharp Tooth"];
+    const withNoises1 = [
+      'Fido says "Woof!"',
+      'Garfield says "Meow!"',
+      'Sharp Tooth says "ROWR."',
     ];
-
-    const cats = [
-      'Garfield',
-      'Heathcliff',
-    ]
-
-    const dinos = [
-      'Barnie',
-      'Sharp Tooth',
-    ]
-
-    const animals1 = ['Fido', 'Garfield', 'Sharp Tooth'];
-    const withNoises1 = ['Fido says "Woof!"', 'Garfield says "Meow!"', 'Sharp Tooth says "ROWR."'];
-    const animals2 = ['Rolph', 'Maisie', 'Barnie', 'Heathcliff'];
-    const withNoises2 = ['Rolph says "Woof!"', 'Maisie says "Woof!"', 'Barnie says "ROWR."', 'Heathcliff says "Meow!"'];
+    const animals2 = ["Rolph", "Maisie", "Barnie", "Heathcliff"];
+    const withNoises2 = [
+      'Rolph says "Woof!"',
+      'Maisie says "Woof!"',
+      'Barnie says "ROWR."',
+      'Heathcliff says "Meow!"',
+    ];
 
     expect(addNoises(animals1)).toEqual(withNoises1);
     expect(addNoises(animals2)).toEqual(withNoises2);
-  })
+  });
 
   it(`doesn't modify the original array`, () => {
-    const animals1 = ['Fido', 'Garfield', 'Sharp Tooth'];
-    const animals2 = ['Rolph', 'Maisie', 'Barnie', 'Heathcliff'];
-    const originalAnimals1 = ['Fido', 'Garfield', 'Sharp Tooth'];
-    const originalAnimals2 = ['Rolph', 'Maisie', 'Barnie', 'Heathcliff'];
+    const animals1 = ["Fido", "Garfield", "Sharp Tooth"];
+    const animals2 = ["Rolph", "Maisie", "Barnie", "Heathcliff"];
+    const originalAnimals1 = ["Fido", "Garfield", "Sharp Tooth"];
+    const originalAnimals2 = ["Rolph", "Maisie", "Barnie", "Heathcliff"];
 
     addNoises(animals1);
     addNoises(animals2);
 
     expect(animals1).toEqual(originalAnimals1);
     expect(animals2).toEqual(originalAnimals2);
-  })
-})
+  });
+});
 
-describe('womenOnly', () => {
+describe("womenOnly", () => {
   it(`returns an array of only those strings in the given array where the last character is "F"`, () => {
-    const people1 = [
-      'Colin - M',
-      'Mesuara - F',
-      'Ginny - F',
-      'Anthony - M',
-    ]
+    const people1 = ["Colin - M", "Mesuara - F", "Ginny - F", "Anthony - M"];
 
-    const answer1 = [
-      'Mesuara - F',
-      'Ginny - F',
-    ]
+    const answer1 = ["Mesuara - F", "Ginny - F"];
 
-    const people2 = [
-      'Ted - M',
-      'Melissa - F',
-      'Biff - M',
-      'Spiffs - F',
-    ]
+    const people2 = ["Ted - M", "Melissa - F", "Biff - M", "Spiffs - F"];
 
-    const answer2 = [
-      'Melissa - F',
-      'Spiffs - F',
-    ]
+    const answer2 = ["Melissa - F", "Spiffs - F"];
 
     expect(womenOnly(people1)).toEqual(answer1);
     expect(womenOnly(people2)).toEqual(answer2);
-  })
+  });
 
   it(`doesn't modify the original array`, () => {
     const originalPeople1 = [
-      'Colin - M',
-      'Mesuara - F',
-      'Ginny - F',
-      'Anthony - M',
-    ]
+      "Colin - M",
+      "Mesuara - F",
+      "Ginny - F",
+      "Anthony - M",
+    ];
 
-    const people1 = [
-      'Colin - M',
-      'Mesuara - F',
-      'Ginny - F',
-      'Anthony - M',
-    ]
+    const people1 = ["Colin - M", "Mesuara - F", "Ginny - F", "Anthony - M"];
 
     const originalPeople2 = [
-      'Ted - M',
-      'Melissa - F',
-      'Biff - M',
-      'Spiffs - F',
-    ]
+      "Ted - M",
+      "Melissa - F",
+      "Biff - M",
+      "Spiffs - F",
+    ];
 
-    const people2 = [
-      'Ted - M',
-      'Melissa - F',
-      'Biff - M',
-      'Spiffs - F',
-    ]
+    const people2 = ["Ted - M", "Melissa - F", "Biff - M", "Spiffs - F"];
 
-    womenOnly(people1)
-    womenOnly(people2)
+    womenOnly(people1);
+    womenOnly(people2);
 
     expect(people1).toEqual(originalPeople1);
     expect(people2).toEqual(originalPeople2);
-  })
-})
+  });
+});
 
-describe('integersOnly', () => {
+describe("integersOnly", () => {
   it(`returns an array of only those numbers in the given array where the number is an integer`, () => {
-    const nums1 = [5, -5, 0.5]
-    const answer1 = [5, -5]
+    const nums1 = [5, -5, 0.5];
+    const answer1 = [5, -5];
     const nums2 = [0.9, 8, -100, 0.3];
     const answer2 = [8, -100];
 
     expect(integersOnly(nums1)).toEqual(answer1);
     expect(integersOnly(nums2)).toEqual(answer2);
-  })
+  });
 
   it(`doesn't modify the original array`, () => {
-    const originalNums1 = [5, 10, 3]
+    const originalNums1 = [5, 10, 3];
 
-    const nums1 = [5, 10, 3]
+    const nums1 = [5, 10, 3];
 
-    const originalNums2 = [8, 10, 24]
+    const originalNums2 = [8, 10, 24];
 
-    const nums2 = [8, 10, 24]
+    const nums2 = [8, 10, 24];
 
-    integersOnly(nums1)
-    integersOnly(nums2)
+    integersOnly(nums1);
+    integersOnly(nums2);
 
     expect(nums1).toEqual(originalNums1);
     expect(nums2).toEqual(originalNums2);
-  })
-})
+  });
+});
